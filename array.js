2fab6628fc5c19c4a1c32f536499b312591f6674
FRANCISCO RODRIGO VIEIRA NOGUEIRA <francisconogueira8987@digitalcollege.internal>
2025-06-10 20:19:20 -0300

    Estudo de array

---
+++ array.js
@@ -0,0 +1,49 @@
+//Lista de itens dos comentários
+//1. Criar
+let nomes = ["João", "Maria", "José", "Ana", "Bia"];
+let carros = [];
+
+let idades = ["20", "30", "40", "50", "60"];
+
+const cidadesArgentina = [
+  "Buenos Aires",
+  "Córdoba",
+  "Rosário",
+  "Mendoza",
+  "La Plata"    ]
+
+  //length retorna o tamanho do array
+//console.log(`Tamanho: ${cidadesArgentina.length}`); 
+
+//2. limpar
+//console.log(idades) 
+
+//3. adcionar elementos 
+//console.log(carros);
+
+//push adiciona elementos no final do array
+//carros.push("Fusca");
+//console.log(carros);
+
+//4. remover elementos
+//pop remove o último elemento do array
+// console.log(nomes);
+// nomes.pop();
+// console.log(nomes);
+// nomes.shift(); // remove o primeiro elemento do array
+// console.log(nomes);
+// console.log(nomes[1])
+// console.log(`${nomes[1]} ${nomes[2]}`); // acessando elementos do array
+
+//5. buscar elementos (filtrar)
+//6. percorrer elementos
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+//forEach percorre todos os elementos do array
+frutas.forEach(function(fruta) {
+    console.log(fruta);
+})
+
+
+
+
+
